b2a5681fe4712b3dd7fc02fc1e8e4f4080ee321c
Anthony <95546821+Azeh0@users.noreply.github.com>
2023-08-24 01:43:01 -0400

    Made a Simple Calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,9 +1,43 @@
-
+import java.util.Scanner;
 public class Calculator {
-
-
     public static void main(String[] args) {
+        Scanner KB = new Scanner(System.in);
 
+        double NumA, NumB, Total = 0;
+        String Operation;
+
+        System.out.println("Welcome to the Calculator!");
+
+        System.out.println("Enter in +, -, *, or /");
+        Operation = KB.nextLine();
+
+        System.out.println("Please Enter in Number 1");
+        NumA = KB.nextDouble();
+
+       System.out.println("Please Enter in Number 2");
+        NumB = KB.nextDouble();
+
+
+
+        switch (Operation) {
+            case "+":
+                Total = NumA + NumB;
+                break;
+            case "-":
+                Total = NumA - NumB;
+                break;
+            case "*":
+                Total = NumA * NumB;
+                break;
+            case "/":
+                Total = NumA / NumB;
+                break;
+            default:
+                System.out.println("Please Choose one of these four options!");
+                break;
+        }
+
+       System.out.println("Your answer is " + Total);
 
 
 
