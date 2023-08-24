7b121d645b80c6f507a784335d3a9640dd4f687e
Anthony <95546821+Azeh0@users.noreply.github.com>
2023-08-24 00:07:55 -0400

    New Start

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner KB = new Scanner(System.in);
+
+        String scale = "";
+        double temperature = 0, unit = 0;
+
+        System.out.println("Enter in C or F");
+
+        scale = KB.nextLine();
+        scale = scale.toLowerCase();
+
+        if(scale.equals("c")){
+            System.out.println("Enter in the temperature");
+            temperature = KB.nextInt();
+            unit = (temperature * 9/5) + 32;
+            System.out.println("That in Fahrenheit is " + unit);
+        }
+        else if(scale.equals("f")){
+            System.out.println("Enter in the temperature");
+            temperature = KB.nextInt();
+            unit = (temperature - 32) * 5/9;
+            System.out.println("That in Celsius is " + unit);
+        }
+        else{
+            System.out.println("Must choose either C or F!");
+        }
+    }
+}
